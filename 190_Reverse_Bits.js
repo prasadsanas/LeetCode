b3f4aab54e8a4b47e160f508501ed8034e14537b
Prasad Sanas <sanasprasad98@gmail.com>
2021-09-21 19:02:36 +0530

    Leetcode 190

---
+++ 190_Reverse_Bits.js
@@ -0,0 +1,14 @@
+var reverseBits = function (n) {
+  var result = 0;
+  var i = 31;
+  var j = 0;
+  while (i >= 0) {
+    if (((A >> j) & 1) == 1 && j <= 31) {
+      result = result + Math.pow(2, i);
+    }
+    j++;
+    i--;
+  }
+
+  return result;
+};
