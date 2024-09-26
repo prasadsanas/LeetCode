96aa8f09ac01f698c03cc9fc07f580c70bcff396
Prasad Sanas <sanasprasad98@gmail.com>
2024-09-26 19:49:13 +0530

    Leetcode recent

---
+++ 1071_Greatest_Common_Divisor_of_Strings.js
@@ -0,0 +1,13 @@
+var gcdOfStrings = function (str1, str2) {
+  if (str1 + str2 !== str2 + str1) {
+    return "";
+  }
+
+  function gcd(a, b) {
+    if (b.length === 0) {
+      return a;
+    }
+    return gcd(b, a.slice(b, a.length % b.length));
+  }
+  return gcd(str1, str2);
+};
